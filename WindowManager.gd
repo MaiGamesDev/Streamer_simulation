e68e490582337cf9f94a331ade57b71035fa7b5f
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-06-02 16:33:12 +0900

    5/31

--- WindowManager.gd
+++ WindowManager.gd
@@ -2,20 +2,17 @@ extends MarginContainer
 
 
 var current_rect_size = Vector2(0,0)
+var is_window_closed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_rect_size = rect_size
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		close()
-
-
 func close():
 	$Tween.interpolate_property(self, "rect_size", rect_size, Vector2(0,0), 0.1, $Tween.TRANS_LINEAR,Tween.EASE_IN)
 	$Tween.start()
 	current_rect_size = rect_size
+	is_window_closed = true
 
 func open():
 	$Tween.interpolate_property(self, "rect_size", rect_size, current_rect_size, 0.1, $Tween.TRANS_LINEAR,Tween.EASE_IN)
@@ -23,8 +20,10 @@ func open():
 
 
 func _on_Minimize_pressed():
-	close()
-
+	if is_window_closed == false:
+		close()
+	else:
+		open()
 
 func _on_Close_pressed():
-	open()
+	queue_free()
